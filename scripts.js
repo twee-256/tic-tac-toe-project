10cfa8d9602df55872269a2db055d4801bef642a
Jesutooni Oguntula <joguntula@gmail.com>
2026-01-30 01:47:46 -0600

    implemented win conditions

--- scripts.js
+++ scripts.js
@@ -38,12 +38,14 @@ const GameController = (function () {
     }
 
     function switchPlayer() {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        console.log(activePlayer);
     }
 
     function placeMark(row, col) {
         board[row][col] = activePlayer.marker;
         console.log(board);
+        checkWin()
         switchPlayer();
     }
 
@@ -61,7 +63,7 @@ const GameController = (function () {
     }
 
     function gameEvent(event) {
-        if (!/^\d$/.test(event.key)) {
+        if (!/^[1-9]$/.test(event.key)) {
             return;
         }
         const num = Number(event.key);
@@ -74,6 +76,67 @@ const GameController = (function () {
     }
 
     window.addEventListener('keydown', gameEvent);
+
+    // check win
+    function checkWin() {
+
+        const rows = board.length;
+        const cols = board[0].length;
+        const isMarker = v => v === "X" || v === "O";
+        // horizontals
+        for (let r = 0; r < rows; r++) {
+            const a = board[r][0];
+            if (isMarker(a) && a == board[r][1] && a == board[r][2]) {
+                console.log("Win!");
+                // return true;
+            }
+        }
+        // return false;
+
+        // verticals
+        for (let c = 0; c < cols - 1; c++) {
+            const a = board[0][c];
+            if (isMarker(a) && a == board[1][c] && a == board[2][c]) {
+                console.log("Win!");
+                // return true;
+            }
+        }
+        // return false;
+
+        // diagonals
+        let arr1 = [];
+        for (let d = 0; d < board.length; d++) {
+            if (isMarker(board[d][d])) {
+                arr1.push(board[d][d]);
+            }
+        }
+        if (arr1.length == 3) {
+            for (let i = 1; i < arr1.length; i++) {
+                if (arr1[i] !== arr1[0]) {
+                    return false;
+                }
+            }
+            console.log("Win!");
+            return true;
+        }
+
+        let arr2 = [];
+        for (let d = 0; d < board.length; d++) {
+            const j = board.length - 1 - d;
+            if (isMarker(board[d][j])) {
+                arr2.push(board[d][j]);
+            }
+        }
+        if (arr2.length == 3) {
+            for (let i = 1; i < arr2.length; i++) {
+                if (arr2[i] !== arr2[0]) {
+                    return false;
+                }
+            }
+            console.log("Win!");
+            return true;
+        }
+    }
 
     return { setPlayers, board };
 }());
