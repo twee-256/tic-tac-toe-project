fc09f185a625b0fae1884dcc8b822f72479b88b9
Jesutooni Oguntula <joguntula@gmail.com>
2026-03-11 15:55:38 -0500

    final touches

--- scripts.js
+++ scripts.js
@@ -1,30 +1,39 @@
 
 // for the board
 function GameBoard() {
-    const board = [];
-    let num = 1;
-    for (let i = 0; i < 3; i++) {
-        board[i] = [];
+    let board = [];
+    const createBoard = () => {
+        board = [];
+        let num = 1;
+        for (let i = 0; i < 3; i++) {
+            board[i] = [];
 
-        for (let j = 0; j < 3; j++) {
-            board[i][j] = num++;
+            for (let j = 0; j < 3; j++) {
+                board[i][j] = num++;
+            }
         }
     }
     const getBoard = () => board;
+    const resetBoard = () => createBoard();
+    createBoard();
 
-    return { getBoard };
+    return { getBoard, resetBoard };
 };
-
-// const gb = GameBoard();
 
 // for the players
 const CreatePlayer = (name, marker) => ({ name, marker });
 
+
+let title = document.querySelector(".title-div");
+title.textContent = "New Game";
+let win = false;
+let gameButton = document.querySelector(".flex-item");
+
 //for the game
 function GameController() {
 
-    const board = GameBoard().getBoard();
-    console.log(board);
+    const gameBoard = GameBoard();
+    let gb = gameBoard.getBoard();
 
     let players = [];
     let activePlayer = null;
@@ -34,27 +43,26 @@ function GameController() {
             CreatePlayer(name1, 'X'),
             CreatePlayer(name2, 'O')
         ];
-        activePlayer = players[0]
+        activePlayer = players[0];
     }
 
     function switchPlayer() {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
-        console.log(activePlayer);
     }
 
     function placeMark(row, col) {
-        board[row][col] = activePlayer.marker;
-        console.log(board);
-        checkWin()
-        switchPlayer();
+        gb[row][col] = activePlayer.marker;
+        checkWin();
+        if (!win) {
+            switchPlayer();
+            title.textContent = activePlayer.name + "'s" + " turn";
+        }
     }
 
     function findCell(num) {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                if (board[i][j] === num) {
-                    console.log("row:", i);
-                    console.log("col:", j);
+        for (let i = 0; i < gb.length; i++) {
+            for (let j = 0; j < gb[i].length; j++) {
+                if (gb[i][j] == num) {
                     return { row: i, col: j };
                 }
             }
@@ -63,10 +71,10 @@ function GameController() {
     }
 
     function gameEvent(event) {
-        if (!/^[1-9]$/.test(event.key)) {
+        if (!/^[1-9]$/.test(event.target.value)) {
             return;
         }
-        const num = Number(event.key);
+        const num = Number(event.target.value);
         const pos = findCell(num);
         if (!pos) {
             return
@@ -75,90 +83,85 @@ function GameController() {
         placeMark(pos.row, pos.col);
     }
 
-    // window.addEventListener('keydown', gameEvent);
+    function resetGame() {
+        gameBoard.resetBoard();
+        gb = gameBoard.getBoard();
+        activePlayer = players[0];
+    }
 
-    // check win
     function checkWin() {
-
-        const rows = board.length;
-        const cols = board[0].length;
+        const rows = gb.length;
+        const cols = gb[0].length;
         const isMarker = v => v === "X" || v === "O";
 
         // horizontals
         for (let r = 0; r < rows; r++) {
-            const a = board[r][0];
-            if (isMarker(a) && a === board[r][1] && a === board[r][2]) {
-                console.log("Win!");
-                // return true;
+            const a = gb[r][0];
+            if (isMarker(a) && a === gb[r][1] && a === gb[r][2]) {
+                win = true;
+                title.textContent = activePlayer.name + " Wins!!!";
             }
         }
-        // return false;
 
         // verticals
-        for (let c = 0; c < cols - 1; c++) {
-            const a = board[0][c];
-            if (isMarker(a) && a === board[1][c] && a === board[2][c]) {
-                console.log("Win!");
-                // return true;
+        for (let c = 0; c < cols; c++) {
+            const a = gb[0][c];
+            if (isMarker(a) && a === gb[1][c] && a === gb[2][c]) {
+                win = true;
+                title.textContent = activePlayer.name + " Wins!!!";
             }
         }
-        // return false;
 
         // diagonals
-        let arr1 = [];
-        for (let d = 0; d < board.length; d++) {
-            if (isMarker(board[d][d])) {
-                arr1.push(board[d][d]);
-            }
-        }
-        if (arr1.length == 3) {
-            for (let i = 1; i < arr1.length; i++) {
-                if (arr1[i] !== arr1[0]) {
-                    return false;
-                }
-            }
-            console.log("Win!");
-            return true;
-        }
+        if (isMarker(gb[0][2])) {
+            if (gb[0][2] === gb[1][1] && gb[1][1] === gb[2][0]) {
+                win = true;
+                title.textContent = activePlayer.name + " Wins!!!";
+            };
+        };
 
-        let arr2 = [];
-        for (let d = 0; d < board.length; d++) {
-            const j = board.length - 1 - d;
-            if (isMarker(board[d][j])) {
-                arr2.push(board[d][j]);
-            }
-        }
-        if (arr2.length == 3) {
-            for (let i = 1; i < arr2.length; i++) {
-                if (arr2[i] !== arr2[0]) {
-                    return false;
-                }
-            }
-            console.log("Win!");
-            return true;
-        }
+        if (isMarker(gb[0][0])) {
+            if (gb[0][0] === gb[1][1] && gb[1][1] === gb[2][2]) {
+                win = true;
+                title.textContent = activePlayer.name + " Wins!!!";
+            };
+        };
+
     }
 
-    return { setPlayers, gameEvent, board, checkWin };
+    return { setPlayers, gameEvent, checkWin, resetGame, getBoard: () => gb };
 };
 
 function ScreenDisplay() {
     const game = GameController();
-    game.setPlayers("Player 1", "Player 2");
+    console.log(game);
+    const playerOne = document.querySelector("#playerone");
+    const playerTwo = document.querySelector("#playertwo");
     const boardDisplay = document.querySelector(".flex-container");
-    // boardDisplay.textContent = "";
+    boardDisplay.classList.add("hidden");
+
+    const headerDisplay = document.querySelector(".header");
+    const start = document.querySelector("#start");
+    const startButton = document.createElement("button");
+    startButton.textContent = "Start Game!";
+
+    start.appendChild(startButton);
+    headerDisplay.append(start);
+
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset!";
+    headerDisplay.append(resetButton);
 
     const UpdateScreen = () => {
         boardDisplay.textContent = "";
-        const board = game.board;
+        const board = game.getBoard();
         console.log(board);
         board.forEach(row => {
-            console.log(row);
             row.forEach(cell => {
-                console.log(cell);
                 const boxButton = document.createElement("button");
                 boxButton.classList.add("flex-item");
                 boxButton.textContent = cell;
+                boxButton.value = cell;
                 if (cell === "X") {
                     boxButton.classList.add("flex-x");
                 }
@@ -170,13 +173,25 @@ function ScreenDisplay() {
         });
     }
 
-    UpdateScreen();
+    startButton.addEventListener("click", () => {
+        game.setPlayers(playerOne.value, playerTwo.value);
+        boardDisplay.classList.remove("hidden");
+        UpdateScreen();
+        title.textContent = playerOne.value + "'s" + " turn";
+    });
 
-    window.addEventListener('keydown', event => {
+
+    resetButton.addEventListener('click', event => {
+        game.resetGame();
+        title.textContent = "New Game";
+        win = false;
+        UpdateScreen();
+    });
+
+    boardDisplay.addEventListener('click', event => {
         game.gameEvent(event);
         UpdateScreen();
     });
 };
 
 ScreenDisplay();
-// GameController.setPlayers("jeff", "mary");
